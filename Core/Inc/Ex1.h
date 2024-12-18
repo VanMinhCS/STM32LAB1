15893df42841f75a9ad4edf264d908af80cb5060
Minh <minhnguyenvancs213@gmail.com>
2024-12-18 23:02:40 +0700

    Ex1

---
+++ Core/Inc/Ex1.h
@@ -0,0 +1,15 @@
+/*
+ * Ex1.h
+ *
+ *  Created on: Dec 18, 2024
+ *      Author: VanMinhCS
+ */
+
+#ifndef INC_EX1_H_
+#define INC_EX1_H_
+
+#include "main.h"
+void Exercise1();
+
+
+#endif /* INC_EX1_H_ */
