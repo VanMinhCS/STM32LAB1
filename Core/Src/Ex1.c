71da71aedf5e66e2a2fabd0f03138ac13391f223
Minh <minhnguyenvancs213@gmail.com>
2024-12-19 16:37:36 +0700

    Ex1

---
+++ Core/Src/Ex1.c
@@ -0,0 +1,17 @@
+/*
+ * Ex1.c
+ *
+ *  Created on: Dec 19, 2024
+ *      Author: VanMinhCS
+ */
+
+
+#include "Ex1.h"
+void Exercise1(){
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, RESET);
+	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, SET);
+	HAL_Delay(2000);
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, SET);
+	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, RESET);
+	HAL_Delay(2000);
+}
